6e97c2018215bd3423ec197071d02a38bf91914c
Shresth Varshney <shresthvarshney980@gmail.com>
2021-06-18 17:19:43 +0530

    Create Training model.py

---
+++ Training model.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+data_path = './faces/user/' 
+onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
+
+Training_Data, Labels = [], []
+
+for i, files in enumerate(onlyfiles):
+    image_path = data_path + onlyfiles[i]
+    images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    Training_Data.append(np.asarray(images, dtype=np.uint8))
+    Labels.append(i)
+
+Labels = np.asarray(Labels, dtype=np.int32)
+
+shresth_model  = cv2.face_LBPHFaceRecognizer.create()
+shresth_model.train(np.asarray(Training_Data), np.asarray(Labels))
+print("Model trained sucessefully")
